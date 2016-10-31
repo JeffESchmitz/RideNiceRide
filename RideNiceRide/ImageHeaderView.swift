284aa8fc9b29257af650edc488c537b7eafea60d
Jeff Schmitz <jeffeschmitz@gmail.com>
2016-10-31 09:38:32 -0500

    BeetleJuice! Happy Halloween

---
+++ RideNiceRide/ImageHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ImageHeaderView.swift
+//  RideNiceRide
+//
+//  Created by Jeff Schmitz on 10/31/16.
+//  Copyright © 2016 Jeff Schmitz. All rights reserved.
+//
+
+import UIKit
+
+class ImageHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
