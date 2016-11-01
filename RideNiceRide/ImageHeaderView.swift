c00ef32515bd7f8588f58ae9449f941af32c5cef
Jeff Schmitz <jeffeschmitz@gmail.com>
2016-11-01 06:32:06 -0500

    Adding ImageHeaderView swift and xib layouts

--- RideNiceRide/ImageHeaderView.swift
+++ RideNiceRide/ImageHeaderView.swift
@@ -10,12 +10,23 @@ import UIKit
 
 class ImageHeaderView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var backgroundImage: UIImageView!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundColor = UIColor(hex: "E0E0E0")
+        self.profileImage.layoutIfNeeded()
+        self.profileImage.layer.cornerRadius = self.profileImage.bounds.size.height / 2
+        self.profileImage.clipsToBounds = true
+        self.profileImage.layer.borderWidth = 1
+        self.profileImage.layer.borderColor = UIColor.white.cgColor
+        self.profileImage.setRandomDownloadImage(80, height: 80)
+
+        // swiftlint:disable todo
+        // TODO: Rework/remove next two lines
+        // swiftlint:enable todo
+        self.backgroundImage.setRandomDownloadImage(Int(self.bounds.size.width), height: 160)
+//        self.backgroundImage.image = UIImage(named: "hero")
+    }
 }
