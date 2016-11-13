8e2825276566b864e214aac405a77a42f997fc71
Jeff Schmitz <jeffeschmitz@gmail.com>
2016-11-12 20:12:38 -0600

    Adding Images.swift

---
+++ RideNiceRide/Enums/Images.swift
@@ -0,0 +1,31 @@
+//
+//  Images.swift
+//  RideNiceRide
+//
+//  Created by Jeff Schmitz on 11/12/16.
+//  Copyright © 2016 Jeff Schmitz. All rights reserved.
+//
+
+// Generated using SwiftGen, by O.Halligon — https://github.com/AliSoftware/SwiftGen
+
+#if os(iOS) || os(tvOS) || os(watchOS)
+  import UIKit.UIImage
+  typealias Image = UIImage
+#elseif os(OSX)
+  import AppKit.NSImage
+  typealias Image = NSImage
+#endif
+
+enum Asset: String {
+  case Menu_Hamburger = "menu-hamburger"
+
+  var image: Image {
+    return Image(asset: self)
+  }
+}
+
+extension Image {
+  convenience init!(asset: Asset) {
+    self.init(named: asset.rawValue)
+  }
+}
