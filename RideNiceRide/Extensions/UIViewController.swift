77b165ef5eed622f27d5c428d155f158fdab1577
Jeff Schmitz <jeffeschmitz@gmail.com>
2016-11-12 20:11:29 -0600

    LeftMenuViewController is finally working.

--- RideNiceRide/Extensions/UIViewController.swift
+++ RideNiceRide/Extensions/UIViewController.swift
@@ -10,19 +10,20 @@ import UIKit
 
 extension UIViewController {
 
-    func setNavigationBarItem() {
-        self.addLeftBarButtonWithImage(UIImage(named: "ic_menu_black_24dp")!)
-        self.addRightBarButtonWithImage(UIImage(named: "ic_notifications_black_24dp")!)
-        self.slideMenuController()?.removeLeftGestures()
-        self.slideMenuController()?.removeRightGestures()
-        self.slideMenuController()?.addLeftGestures()
-        self.slideMenuController()?.addRightGestures()
-    }
+  func setNavigationBarItem() {
+//    self.addLeftBarButtonWithImage(UIImage(named: "ic_menu_black_24dp")!)
+//    self.addLeftBarButtonWithImage(#imageLiteral(resourceName: "menu-hamburger"))
+    self.addLeftBarButtonWithImage(Asset.Menu_Hamburger.image)
+    self.slideMenuController()?.removeLeftGestures()
+    self.slideMenuController()?.removeRightGestures()
+    self.slideMenuController()?.addLeftGestures()
+    self.slideMenuController()?.addRightGestures()
+  }
 
-    func removeNavigationBarItem() {
-        self.navigationItem.leftBarButtonItem = nil
-        self.navigationItem.rightBarButtonItem = nil
-        self.slideMenuController()?.removeLeftGestures()
-        self.slideMenuController()?.removeRightGestures()
-    }
+  func removeNavigationBarItem() {
+    self.navigationItem.leftBarButtonItem = nil
+    self.navigationItem.rightBarButtonItem = nil
+    self.slideMenuController()?.removeLeftGestures()
+    self.slideMenuController()?.removeRightGestures()
+  }
 }
