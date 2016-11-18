3ff27ed9caeb71e432e84d2710af42dbcfaffb83
Jeff Schmitz <jeffeschmitz@gmail.com>
2016-11-18 07:59:03 -0600

    Implemented BottomViewController

--- RideNiceRide/ViewControllers/PullUp/BottomViewController.swift
+++ RideNiceRide/ViewControllers/PullUp/BottomViewController.swift
@@ -7,29 +7,117 @@
 //
 
 import UIKit
+import ISHPullUp
+import MapKit
+import GoogleMaps
+import Willow
 
 class BottomViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  // swiftlint:disable variable_name
+  let log = Logger()
+  // swiftlint:enable variable_name
 
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var handleView: ISHPullUpHandleView!
+  @IBOutlet weak var rootView: UIView!
+  @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet weak var topLabel: UILabel!
+  @IBOutlet weak var topView: UIView!
+  @IBOutlet weak var panoramaView: UIView!
+
+  var firstAppearanceCompleted = false
+  weak var pullUpController: ISHPullUpViewController!
+
+  // allow the pullup to snap to the half-way point
+  var halfWayPoint = CGFloat(0)
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let frameRect = CGRect(x: 0, y: 0, width: 375, height: 128)
+
+    let panoView = GMSPanoramaView(frame: frameRect)
+    self.panoramaView.frame = panoView.frame
+    self.panoramaView.addSubview(panoView)
+    self.panoramaView = panoView
+
+    let testCoordinate = CLLocationCoordinate2DMake(-33.732, 150.312)
+    panoView.moveNearCoordinate(testCoordinate)
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    firstAppearanceCompleted = true
+  }
+
+  private dynamic func handleTapGesture(gesture: UITapGestureRecognizer) {
+    if pullUpController.isLocked {
+      return
+    }
+    pullUpController.toggleState(animated: true)
+  }
+}
+
+extension BottomViewController: GMSPanoramaViewDelegate {
+  // MARK: - GMSPanoramaViewDelegate
+  func panoramaView(_ view: GMSPanoramaView, error: Error, onMoveNearCoordinate coordinate: CLLocationCoordinate2D) {
+    log.debug {"Moving near coordinate (\(coordinate.latitude),\(coordinate.longitude) error: \(error.localizedDescription)"}
+  }
+
+  func panoramaView(_ view: GMSPanoramaView, error: Error, onMoveToPanoramaID panoramaID: String) {
+    log.debug("Moving to PanoID \(panoramaID) error: \(error.localizedDescription)")
+  }
+
+  func panoramaView(_ view: GMSPanoramaView, didMoveTo panorama: GMSPanorama?) {
+    log.debug("Moved to panoramaID: \(panorama?.panoramaID) coordinates: (\(panorama?.coordinate.latitude),\(panorama?.coordinate.longitude))")
+  }
+}
+
+extension BottomViewController: ISHPullUpSizingDelegate {
+  func pullUpViewController(_ pullUpViewController: ISHPullUpViewController, maximumHeightForBottomViewController bottomVC: UIViewController, maximumAvailableHeight: CGFloat) -> CGFloat {
+    let totalHeight = rootView.systemLayoutSizeFitting(UILayoutFittingCompressedSize).height
+
+    // Allow the pullUp to snap to the half way point, calculate the cached
+    // value here and perform the snapping in ..targetHeightForBottomViewController..
+    halfWayPoint = totalHeight / 2.0
+    return totalHeight
+  }
+
+  func pullUpViewController(_ pullUpViewController: ISHPullUpViewController, minimumHeightForBottomViewController bottomVC: UIViewController) -> CGFloat {
+    return topView.systemLayoutSizeFitting(UILayoutFittingCompressedSize).height
+  }
+
+  func pullUpViewController(_ pullUpViewController: ISHPullUpViewController, targetHeightForBottomViewController bottomVC: UIViewController, fromCurrentHeight height: CGFloat) -> CGFloat {
+    // if around 30pt of the halfway point -> snap to it
+    if abs(height - halfWayPoint) < 30 {
+      return halfWayPoint
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // default
+    return height
+  }
+
+  func pullUpViewController(_ pullUpViewController: ISHPullUpViewController, update edgeInsets: UIEdgeInsets, forBottomViewController contentVC: UIViewController) {
+    scrollView.contentInset = edgeInsets
+  }
+}
+
+extension BottomViewController: ISHPullUpStateDelegate {
+  func pullUpViewController(_ pullUpViewController: ISHPullUpViewController, didChangeTo state: ISHPullUpState) {
+    topLabel.text = textForState(state)
+    handleView.setState(ISHPullUpHandleView.handleState(for: state), animated: firstAppearanceCompleted)
+  }
+
+  private func textForState(_ state: ISHPullUpState) -> String {
+    switch state {
+    case .collapsed:
+      return "Drag up or tap"
+    case .intermediate:
+      return "Intermediate"
+    case .dragging:
+      return "Hold on"
+    case .expanded:
+      return "Drag down or tap"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
 }
