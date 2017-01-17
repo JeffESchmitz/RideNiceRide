23ae60012da97b1d5736f18791f16de577adc6fb
Jeff Schmitz <jeffeschmitz@gmail.com>
2017-01-16 22:31:57 -0600

    set the favorite map position, centered and zoomed, might still need a bit more zoom.

--- RideNiceRide/Views/FavoritesTableViewCell.swift
+++ RideNiceRide/Views/FavoritesTableViewCell.swift
@@ -13,69 +13,90 @@ import MapKit
 import Willow
 
 class FavoritesTableViewCell: UITableViewCell {
-
+  
   class var identifier: String { return String.className(self) }
-
+  
   @IBOutlet weak var addressLabel: UILabel!
   @IBOutlet weak var nameLabel: UILabel!
   @IBOutlet weak var bikesValueLabel: UILabel!
   @IBOutlet weak var racksValueLabel: UILabel!
-//  @IBOutlet weak var panoramaView: UIView!
+  //  @IBOutlet weak var panoramaView: UIView!
   @IBOutlet weak var mapView: MKMapView!
-
+  
   // swiftlint:disable missing_docs
   public required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
     setup()
   }
   // swiftlint:enable missing_docs
-
+  
   override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
     setup()
   }
-
+  
   override func awakeFromNib() {
     super.awakeFromNib()
     
-//    initialize()
+    //    initialize()
   }
-
+  
   open func setup() {
   }
-
-//  open func initialize() {
-//    let frameRect = CGRect(x: 0, y: 0, width: 375, height: 104)
-//    let panoView = GMSPanoramaView(frame: frameRect)
-//
-////    self.panoramaView.frame = panoView.frame
-//    self.panoramaView.addSubview(panoView)
-//    self.panoramaView = panoView
-//
-//    let testCoordinate = CLLocationCoordinate2DMake(-33.732, 150.312)
-//    panoView.moveNearCoordinate(testCoordinate)
-//  }
-
-
+  
+  //  open func initialize() {
+  //    let frameRect = CGRect(x: 0, y: 0, width: 375, height: 104)
+  //    let panoView = GMSPanoramaView(frame: frameRect)
+  //
+  ////    self.panoramaView.frame = panoView.frame
+  //    self.panoramaView.addSubview(panoView)
+  //    self.panoramaView = panoView
+  //
+  //    let testCoordinate = CLLocationCoordinate2DMake(-33.732, 150.312)
+  //    panoView.moveNearCoordinate(testCoordinate)
+  //  }
+  
+  
   override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
-
+    
     // Configure the view for the selected state
   }
-
-
+  
+  
   var favoriteStation: FavoriteStation? {
     didSet {
       self.reloadData()
     }
   }
-
+  
   func reloadData() {
     if let favoriteStation = self.favoriteStation {
-        self.addressLabel.text = favoriteStation.stAddress1
-        self.nameLabel.text = favoriteStation.stationName
-        self.bikesValueLabel.text = favoriteStation.availableBikes
-        self.racksValueLabel.text = favoriteStation.totalDocks
+      if let favoriteLat = favoriteStation.latitude, let favoriteLong = favoriteStation.longitude {
+          if let latitude = CLLocationDegrees(favoriteLat), let longitude = CLLocationDegrees(favoriteLong) {
+            setMapLocation(latitude: latitude, longitude: longitude, zoom: 0.005)
+        }
+      }
+      self.addressLabel.text = favoriteStation.stAddress1
+      self.nameLabel.text = favoriteStation.stationName
+      self.bikesValueLabel.text = favoriteStation.availableBikes
+      self.racksValueLabel.text = favoriteStation.totalDocks
     }
+  }
+  
+//  func setMapLocation(mapView: MKMapView, latitude: CLLocationDegrees, longitude: CLLocationDegrees, zoom: Double = 1) {
+  func setMapLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, zoom: Double = 1) {
+  
+    // define the map zoom span
+    let latitudZoomLevel: CLLocationDegrees = zoom
+    let longitudZoomLevel: CLLocationDegrees = zoom
+    let zoomSpan: MKCoordinateSpan = MKCoordinateSpanMake(latitudZoomLevel, longitudZoomLevel)
+    
+    // use latitud and longitud to create a location coordinate
+    let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+    
+    // define and set the region of our map using the zoom map and location
+    let region: MKCoordinateRegion = MKCoordinateRegionMake(location, zoomSpan)
+    mapView.setRegion(region, animated: true)
   }
 }
